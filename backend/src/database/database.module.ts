843d4d184f2b493960878570e0aa57f603d5272d
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-09-26 18:04:07 -0300

    BACKEND: Configured TypeORM

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,4 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.register.KEY],
+      useFactory: (configService: ConfigType<typeof config.register>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+          synchronize: false,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
