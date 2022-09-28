99854f609a5da5d82edc0ffbfb064455c91031e8
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-09-28 10:02:33 -0300

    FRONTEND: Created useInput to handler the inputs

---
+++ frontend/hooks/useInput.tsx
@@ -0,0 +1,21 @@
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+interface Params<T> {
+  value?: T;
+  label?: string;
+  extraOpt: Object;
+}
+export default function useInput<T>(params: Params<T>) {
+  const [value, setValue] = useState<T>(params.value || null);
+  const input = (
+    <TextField
+      label={params.label}
+      value={value}
+      onChange={(e) => setValue(e.target.value as T)}
+      {...params.extraOpt}
+    />
+  );
+  // Change as any
+  return [value, input as any, setValue];
+}
